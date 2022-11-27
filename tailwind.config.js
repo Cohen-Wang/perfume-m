a0883d6470fedecafe99e12f9ae601e9059d1e31
wanghaitao <615940742@qq.com>
2022-11-27 23:52:13 +0800

    feat: 安装tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  content: [],
+  content: [["./src/**/*.{html,js}"]],
   theme: {
     extend: {}
   },
