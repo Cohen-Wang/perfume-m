dd43addf5a8c305768ee6442982dc89e00a1ad15
wanghaitao <615940742@qq.com>
2022-11-27 23:33:23 +0800

    feat: 安装tailwind.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+// 样式
 import './style/style.css'
+import './style/tailwind.css'
 import { create, NButton} from 'naive-ui'
 import App from './App.vue'
+
 
 const naive = create({
   components: [NButton]
