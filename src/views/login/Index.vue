0606f25852c6b3ad2fba0299e4c75bbc44c23caf
wanghaitao <615940742@qq.com>
2022-12-20 20:11:53 +0800

    feat: 优化

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,20 +1,72 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import { reactive, ref } from 'vue'
+import type { FormInst, FormRules } from 'naive-ui'
 // +--------------------------------------------------
 // | data
 // +--------------------------------------------------
-
+const form = reactive({
+	username: '',
+	password: ''
+})
+const rules: FormRules = {
+	username: [
+		{ required: true, message: '请输入用户名', trigger: 'blur' }
+	],
+	password: [
+		{ required: true, message: '请输入用户名', trigger: 'blur' }
+	]
+}
+// DOM
+const FormRef = ref<FormInst | null>(null)
 // +--------------------------------------------------
 // | methods
 // +--------------------------------------------------
+// 点击确认
+const onConfirmClick = (): void => {
+	FormRef.value?.validate((errors: any) => {
+		if (errors) return
+		console.log('form', form)
+	})
+}
 </script>
 
 <template>
-  <div class="w-full h-screen bg">登录</div>
+  <div class="w-full h-screen bg flex justify-center items-center">
+		<div class="w-1/5 p-3 shadow-md">
+			<!-- 标题 -->
+			<n-divider dashed>登录</n-divider>
+			<!-- 表单 -->
+			<n-form ref="FormRef" :model="form" :rules="rules">
+				<!-- 用户名 -->
+				<n-form-item path="username" label="用户名">
+					<n-input
+						v-model:value="form.username"
+						minlength="8"
+						maxlength="30"
+						show-count
+						placeholder="请输入"
+						@keydown.enter.prevent
+					/>
+				</n-form-item>
+				<!-- 密码 -->
+				<n-form-item path="password" label="密码">
+					<n-input
+						v-model:value="form.password"
+						minlength="8"
+						maxlength="30"
+						show-count
+						placeholder="请输入"
+						@keydown.enter.prevent
+					/>
+				</n-form-item>
+			</n-form>
+			<n-button type="primary" class="w-full" @click="onConfirmClick">确认</n-button>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .bg {
-	background: #f0f2f5 url(../../assets/svg/login.svg) no-repeat center 100px/100% ;
+	background: #ffffff url(../../assets/svg/login.svg) no-repeat center 100px/100% ;
 }
 </style>
