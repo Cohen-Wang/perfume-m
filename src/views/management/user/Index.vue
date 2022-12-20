d976ccdbb346c170775bfe79bfc717892513b656
wanghaitao <615940742@qq.com>
2022-12-20 17:54:41 +0800

    feat: 优化

--- src/views/management/user/Index.vue
+++ src/views/management/user/Index.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
 import { reactive, onMounted } from 'vue'
+import { useNotification } from 'naive-ui'
 import type { DataTableColumns } from 'naive-ui'
 import { IUser } from '@/type/interface/user'
 import { DEFAULT_COLUMNS, DEFAULT_TABLE_DATA } from '@/constants/user'
 // +--------------------------------------------------
 // | data
 // +--------------------------------------------------
+// 辅助
+const notification = useNotification()
+// 数据
 const columns: DataTableColumns<IUser> = DEFAULT_COLUMNS
 const tableData: IUser[] = reactive([])
 // +--------------------------------------------------
@@ -27,12 +31,20 @@ const getList = (): void => {
     tableData.push(...DEFAULT_TABLE_DATA)
   }, 1000)
 }
+// 点击新增
+const onAddClick = (): void => {
+  notification.info({
+    title: '123',
+    content: '333333333',
+    duration: 2000
+  })
+}
 </script>
 
 <template>
   <!-- 控制栏 -->
   <div class="p-1 test-border mb-3">
-    <n-button type="info">新增</n-button>
+    <n-button type="info" @click="onAddClick">新增</n-button>
   </div>
   <!-- 表格 -->
   <n-data-table
