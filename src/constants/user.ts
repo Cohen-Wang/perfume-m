a007cb110fa1393c20ea667251bf38fe2cbf8fa9
wanghaitao <615940742@qq.com>
2022-12-20 23:32:04 +0800

    feat: 优化

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,6 +1,8 @@
 import { h } from 'vue'
-import { NButton, NSpace } from 'naive-ui'
+import { NButton, NSpace, NTag } from 'naive-ui'
+import moment from 'moment'
 import type { IUser } from '@/type/interface/user'
+import { EStatus } from '@/type/enum/common'
 // +--------------------------------------------------
 // | module - user
 // +--------------------------------------------------
@@ -20,6 +22,29 @@ export const DEFAULT_COLUMNS = [
   {
     title: '姓名',
     key: 'realName'
+  },
+  {
+    title: '状态',
+    key: 'status',
+    render(row: IUser) {
+      return h(NTag, {
+        type: 'success'
+      }, () => row.status)
+    }
+  },
+  {
+    title: '创建时间',
+    key: 'createTime',
+    render(row: IUser) {
+      return moment(row.createTime).format('YYYY-MM-DD HH:mm:ss')
+    }
+  },
+  {
+    title: '修改时间',
+    key: 'updateTime',
+    render(row: IUser) {
+      return moment(row.updateTime).format('YYYY-MM-DD HH:mm:ss')
+    }
   },
   {
     title: '操作',
@@ -70,6 +95,9 @@ export const DEFAULT_TABLE_DATA: IUser[] = [
     realName: '张三',
     email: 'zhangsan@qq.com',
     mobile: '13612345678',
+    status: EStatus.START,
+    createTime: Date.now(),
+    updateTime: Date.now(),
   },
   {
     id: '1',
--- src/type/interface/user.ts
+++ src/type/interface/user.ts
@@ -1,3 +1,4 @@
+import { EStatus } from '@/type/enum/common'
 // +--------------------------------------------------
 // | module - user
 // +--------------------------------------------------
@@ -8,6 +9,9 @@ export interface IUser {
   id?: string // id
   username: string // 名称
   realName: string // 姓名
+  status: EStatus // 状态
   email?: string // 邮箱
   mobile?: string // 手机号
+  createTime: Date // 创建日期
+  updateTime: Date // 修改
 }
