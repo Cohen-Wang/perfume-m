60279a725c635ecf295d109393eb1b9f09806291
wanghaitao <615940742@qq.com>
2022-12-21 21:50:10 +0800

    feat: 优化

--- src/components/layout/components/NavBar.vue
+++ src/components/layout/components/NavBar.vue
@@ -1,32 +1,29 @@
 <script lang="ts" setup>
+import { useNotification } from 'naive-ui'
 import { MailOutline  } from '@vicons/ionicons5'
 // +--------------------------------------------------
 // | data
 // +--------------------------------------------------
+const notification = useNotification()
 const options = [
-	{
-		label: '滨海湾金沙，新加坡',
-		key: 'marina bay sands',
-		disabled: true
-	},
-	{
-		label: '布朗酒店，伦敦',
-		key: "brown's hotel, london"
-	},
-	{
-		label: '亚特兰蒂斯巴哈马，拿骚',
-		key: 'atlantis nahamas, nassau'
-	},
-	{
-		label: '比佛利山庄酒店，洛杉矶',
-		key: 'the beverly hills hotel, los angeles'
-	}
+	{ label: '个人中心', key: 'personalCenter'},
+	{ label: '退出登录', key: 'logout' }
 ]
 // +--------------------------------------------------
 // | methods
 // +--------------------------------------------------
-const handleSelect  = (key: string | number): void => {
-	// message.info(String(key))
+const handlePersonalCenter = (): void => {
+	notification.error({ title: '暂未开发' })
+}
+const handleLogout = (): void => {
+	notification.error({ title: '暂未开发' })
+}
+const config: any = {
+	personalCenter: handlePersonalCenter,
+	logout: handleLogout,
+}
+const handleSelect  = (key: string): void => {
+	config[key]?.()
 }
 </script>
 
