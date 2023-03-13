c3349daa68e42c1040df360d5a015b5c18e4d223
王海涛 <615940742@qq.com>
2023-03-13 13:41:58 +0800

    feat: 优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,57 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
 // 插件
 import vitePluginEslint from 'vite-plugin-eslint'
 // import vitePluginStylelint from 'vite-plugin-stylelint'
-
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src') // 必须依赖 npm install @types/node --save-dev
-    }
-  },
-  // 代理服务
-  server: {
-    proxy: {
-      '/api': {
-        target: '',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-    // 压缩相关
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
+// +--------------------------------------------------
+// | 实例化
+// +--------------------------------------------------
+// @ts-ignore
+export default defineConfig(({ mode }: any) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src') // 必须依赖 npm install @types/node --save-dev
       }
     },
-    // 打包路径相关
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-        assetFileNames: '[ext]/[name]-[hash].[ext]',
+    // 代理服务
+    server: {
+      proxy: {
+        host: '0.0.0.0',
+        port: env.VITE_APP_PORT || 3000,
+        '/api': {
+          target: '',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    // vitePluginEslint(),
-    // vitePluginStylelint({
-    //   fix: true
-    // })
-  ]
+    },
+    build: {
+      // 压缩相关
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      },
+      // 打包路径相关
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'js/[name]-[hash].js',
+          entryFileNames: 'js/[name]-[hash].js',
+          assetFileNames: '[ext]/[name]-[hash].[ext]',
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      // vitePluginEslint(),
+      // vitePluginStylelint({
+      //   fix: true
+      // })
+    ]
+  }
 })
